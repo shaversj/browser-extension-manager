e40d9ca9888dfa5c593a9cf4b8d2baabc242c971
Josh Shavers <shaversj@gmail.com>
2025-06-26 15:43:28 -0500

    Add Card Component + React

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
+
+export default function Toggle() {
+  const [enabled, setEnabled] = useState(false);
+
+  return (
+    <Switch
+      checked={enabled}
+      onChange={setEnabled}
+      className="group inline-flex h-[20px] w-[36px] items-center rounded-full bg-gray-200 transition data-checked:bg-red-700"
+    >
+      <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-checked:translate-x-4.5" />
+    </Switch>
+  );
+}
