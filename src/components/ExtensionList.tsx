01d198c93908785ac187741bf38068f2851aaaa0
Josh Shavers <shaversj@gmail.com>
2025-07-03 12:52:35 -0500

    Update

--- src/components/ExtensionList.tsx
+++ src/components/ExtensionList.tsx
@@ -10,7 +10,7 @@ export default function ExtensionList() {
   return (
     <div className={"min-h-screen"}>
       <ExtensionListHeader
-        title={"Extension List"}
+        title={"Extensions List"}
         selectedFilter={filter}
         filterChangeHandler={changeFilter}
       />
