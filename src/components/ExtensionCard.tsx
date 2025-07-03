ec13ce2c8f0be4231ed35043a1ecab1a09b7b5e3
Josh Shavers <shaversj@gmail.com>
2025-07-03 07:41:18 -0500

    Add cursor pointer

--- src/components/ExtensionCard.tsx
+++ src/components/ExtensionCard.tsx
@@ -40,14 +40,14 @@ export default function ExtensionCard({
       <div className="flex shrink-0 items-center justify-between">
         <button
           onClick={() => removeHandler(id)}
-          className="text-preset-6 hover:text-neutral-0 dark:text-neutral-0 rounded-full border border-neutral-300 px-4 py-2 text-neutral-900 transition-colors duration-200 hover:bg-red-700 dark:border-neutral-600"
+          className="text-preset-6 hover:text-neutral-0 dark:text-neutral-0 cursor-pointer rounded-full border border-neutral-300 px-4 py-2 text-neutral-900 transition-colors duration-200 hover:bg-red-700 dark:border-neutral-600"
         >
           Remove
         </button>
         <Switch
           checked={enabled}
           onChange={() => onChangeHandler()}
-          className="group inline-flex h-[20px] w-[36px] items-center rounded-full bg-gray-200 transition data-checked:bg-red-700 dark:bg-neutral-600 dark:data-checked:bg-red-400"
+          className="group inline-flex h-[20px] w-[36px] cursor-pointer items-center rounded-full bg-gray-200 transition data-checked:bg-red-700 dark:bg-neutral-600 dark:data-checked:bg-red-400"
         >
           <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-checked:translate-x-4.5" />
         </Switch>
